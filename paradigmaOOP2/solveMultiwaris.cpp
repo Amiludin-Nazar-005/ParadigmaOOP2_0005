a9bfa669214195490ad1a2a7bfad12fdfe6dd5b4
Amiludin-Nazar-005 <amiludin.nazar.ft24@mail.umy.ac.id>
2025-06-02 11:03:01 +0700

    solveMultiwaris

---
+++ paradigmaOOP2/solveMultiwaris.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+class orang
+{
+public:
+    int umur;
+
+    orang(int pUmur) : umur(pUmur)
+    {
+        cout << "orang di buat dengan umur " << umur << "\n"
+             << endl;
+    }
+};
+
+class pekerja : public orang
+{
+public:
+    pekerja(int pUmur) : orang(pUmur)
+    {
+        cout << "pekerja di buat\n"
+             << endl;
+    }
+};
+
+class pelajar : public orang
+{
+public:
+    pelajar(int pUmur) : orang(pUmur)
+    {
+        cout << "pelajar di buat \n"
+             << endl;
+    }
+};
+
+class budi : public pekerja, public pelajar
+{
+public:
+    budi(int pUmur) : pekerja(pUmur),
+                      pelajar(pUmur)
+    {
+        cout << " budi di buat \n"<< endl;
+    }
+};
+
+int main (){
+    budi a(12);
+
+    return 0;
+}
