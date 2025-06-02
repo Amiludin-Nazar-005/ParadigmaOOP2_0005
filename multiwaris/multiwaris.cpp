4410e9470713e13f9b4408a7351b51f210c9d42c
Amiludin-Nazar-005 <amiludin.nazar.ft24@mail.umy.ac.id>
2025-06-02 10:28:25 +0700

    membuat class pekerja dan virtual public orang

--- multiwaris/multiwaris.cpp
+++ multiwaris/multiwaris.cpp
@@ -8,6 +8,16 @@ public:
 
     orang(int pUmur) : umur(pUmur)
     {
-        cout << "orang di buat dengan umur " << umur << "\n" << endl;
+        cout << "orang di buat dengan umur " << umur << "\n"
+             << endl;
+    }
+};
+
+class pekerja : virtual public orang
+{
+public:
+    pekerja(int pUmur) : orang(pUmur)
+    {
+        cout << "pekerja di buat\n" << endl;
     }
 };
