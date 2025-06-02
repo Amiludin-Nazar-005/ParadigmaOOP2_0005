da1115b914868f94fdf354be3abc071f5a5334c9
Amiludin-Nazar-005 <amiludin.nazar.ft24@mail.umy.ac.id>
2025-06-02 09:59:19 +0700

    membuat class public

--- abstraksi/Abstraksi.cpp
+++ abstraksi/Abstraksi.cpp
@@ -3,4 +3,17 @@ using namespace std;
 
 class AbstraksiKlas {
     private : string x, y;
+
+    public :
+        // method untuk mengisi nilai
+        // privat member
+        void setXY(string a, string b){
+            x = a;
+            y = b;
+        }
+        // menampilkan nilai
+        void display (){
+            cout << "x = "<< x << endl;
+            cout << "y = "<< y << endl;
+        }
 };
